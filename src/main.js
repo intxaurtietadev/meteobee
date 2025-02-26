952bb416bb426ea60438728ec53203a2489136c5
igoribon <igoribon@gmail.com>
2025-02-26 11:28:42 +0100

    avanzando

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from '../src/router';
+import {createPinia} from 'pinia';
 
 const app = createApp(App);
 app.use(router); 
 app.mount('#app');
+
+app.use(createPinia());
