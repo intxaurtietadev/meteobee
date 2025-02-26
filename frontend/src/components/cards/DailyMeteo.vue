f2362b214a5fef685d432e10333589a09e9386ff
igoribon <igoribon@gmail.com>
2025-02-26 09:37:03 +0100

    Ya recojo datos de AEMET

--- frontend/src/components/cards/DailyMeteo.vue
+++ frontend/src/components/cards/DailyMeteo.vue
@@ -32,8 +32,10 @@
   <p>El municipio seleccionado es: {{ municipioSelected }}</p>
 </template>
 
+
+
 <script setup>
-import { ref, computed, onMounted } from "vue";
+import { ref, computed, onMounted, watch } from "vue";
 import provinciasData from "../../assets/Provincias.json";
 import municipiosData from "../../assets/Municipios.json";
 
@@ -42,6 +44,29 @@ const provincias = ref([]);
 const municipios = ref([]);
 const provinciaSelected = ref(null);
 const municipioSelected = ref(null);
+const result = ref(null);
+const response = ref(null);
+
+const weatherData = ref(null);
+
+
+
+
+// const fetchData = async () => {
+//   try {
+//     const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+//     const apiUrl = `https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/${encodeURIComponent(municipioSelected.value)}?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzZ20ubmVyZWFAZ21haWwuY29tIiwianRpIjoiNTZjZDU1NTEtMjJhOS00Yzk0LWE1NDAtMTdmZDkxZjY5OGYyIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE3NDAwNTYwMTMsInVzZXJJZCI6IjU2Y2Q1NTUxLTIyYTktNGM5NC1hNTQwLTE3ZmQ5MWY2OThmMiIsInJvbGUiOiIifQ.Zw95iuaxZ6Ggso8KFtFURogSvIT17uCbKXlHsVtScKc`;
+    
+//     const response = await fetch(proxyUrl + apiUrl);
+//     const data = await response.json();
+//     result.value = data;
+//   } catch (error) {
+//     console.error("Error al obtener datos:", error);
+//   }
+// };
+
+
+
 
 //Convertimos los json en arrays y los ordenamos alfabéticamente según el idioma ES
 onMounted(() => {
@@ -65,6 +90,36 @@ const filteredMunicipios = computed(() => {
 const handleProvinciaChange = (event) => {
   provinciaSelected.value = event.target.value;
 };
+
+watch(municipioSelected, async (newValue) => {
+  if (newValue) {
+    await fetchWeatherData(newValue);
+  }
+});
+
+
+// Función para obtener el clima de AEMET
+const fetchWeatherData = async (codigoMunicipio) => {
+  weatherData.value = "Cargando...";
+
+  try {
+    const url = `https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/${codigoMunicipio}?api_key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzZ20ubmVyZWFAZ21haWwuY29tIiwianRpIjoiNTZjZDU1NTEtMjJhOS00Yzk0LWE1NDAtMTdmZDkxZjY5OGYyIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE3NDAwNTYwMTMsInVzZXJJZCI6IjU2Y2Q1NTUxLTIyYTktNGM5NC1hNTQwLTE3ZmQ5MWY2OThmMiIsInJvbGUiOiIifQ.Zw95iuaxZ6Ggso8KFtFURogSvIT17uCbKXlHsVtScKc`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.datos) {
+      const weatherResponse = await fetch(data.datos);
+      weatherData.value = await weatherResponse.json();
+    } else {
+      weatherData.value = "No se encontraron datos.";
+    }
+  } catch (error) {
+    weatherData.value = "Error al obtener datos.";
+    console.error(error);
+  }
+};
+
+
 </script>
 
 
