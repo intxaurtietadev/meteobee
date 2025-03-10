c0206224ee9f7531125d63b8606da3ffa2be12bd
igoribon <igoribon@gmail.com>
2025-03-10 09:03:58 +0100

    Cambio a grants bueno

--- frontend/src/views/GrantsView.vue
+++ frontend/src/views/GrantsView.vue
@@ -1,45 +1,46 @@
 <template>
-    <div class="grants">
-      <h1 class="grants__title">Ayudas</h1>
-      <div class="grants__list">
-        <GrantsCard v-for="grant in grants" :key="grant.id" :grants="grant" />
-      </div>
+  <div class="grants">
+    <h1 class="grants__title">Copia</h1>
+    <div class="grants__list">
+      <GrantsCard v-for="grant in grants" :key="grant.id" :grants="grant" />
     </div>
-  </template>
-  
-  <script setup>
-  import { ref, onMounted } from 'vue';
-  import GrantsCard from '../components/Grants.vue';
-  
-  const grants = ref([]);
-  
-  const fetchGrants = async () => {
-    const response = await fetch('../../public/Grants.json');
-    const data = await response.json();
-    grants.value = data;
-  };
-  
-  onMounted(fetchGrants);
-  </script>
-  
-  <style scoped>
-  .grants {
-    padding-top: 7rem;
-    max-width: 60rem;
-    margin: auto;
-    font-family: var(--font-family);
-  }
-  
-  .grants__title {
-    font-size: 2rem;
-    text-align: center;
-    margin-bottom: 1rem;
-  }
-  
-  .grants__list {
-    display: flex;
-    flex-direction: column;
-    gap: 1.5rem;
-  }
-  </style>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import GrantsCard from '../components/Grantscopy.vue';
+
+const grants = ref([]);
+
+const fetchGrants = async () => {
+  const response = await fetch('http://localhost:3000/grants');
+  if (!response.ok) throw new Error('Error al obtener usuarios.');
+  const data = await response.json();
+  grants.value = data;
+};
+
+onMounted(fetchGrants);
+</script>
+
+<style scoped>
+.grants {
+  padding-top: 7rem;
+  max-width: 60rem;
+  margin: auto;
+  font-family: var(--font-family);
+}
+
+.grants__title {
+  font-size: 2rem;
+  text-align: center;
+  margin-bottom: 1rem;
+}
+
+.grants__list {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+}
+</style>
 
