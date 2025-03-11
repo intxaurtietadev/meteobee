ec0a174da76d17127fe4fd9a8b1d3a9fc6f474ee
nereasaga <sgm.nerea@gmail.com>
2025-03-11 11:35:29 +0100

    arreglado advice temp y cambio snow en advice.json, cambios cypress

--- frontend/src/components/cards/DailyToDo.vue
+++ frontend/src/components/cards/DailyToDo.vue
@@ -72,21 +72,20 @@ const getAdvice = (parameter, value, adviceJSON) => {
   const adviceItem = adviceList.find(item => 
     value >= item.range.min && value <= item.range.max
   );
-  return adviceItem ? adviceItem.advice : ""; // Devuelve "" si no hay recomendación
+  return adviceItem ? adviceItem.advice : ""; 
 };
 
 // Function to find the advice for temperature based on min and max values
 const getTemperatureAdvice = (minTemp, maxTemp, adviceJSON) => {
-  const avgTemp = (minTemp + maxTemp) / 2; // Calcula la temperatura promedio
   const adviceList = adviceJSON.advice["temperature_combined"];
-  
+
   const adviceItem = adviceList.find(item => 
-    avgTemp >= item.range.min && avgTemp <= item.range.max
+    minTemp >= item.range.max_min[0] && minTemp <= item.range.max_min[1] &&
+    maxTemp >= item.range.max_max[0] && maxTemp <= item.range.max_max[1]
   );
 
-  return adviceItem ? adviceItem.advice : ""; // Devuelve "" si no hay recomendación
+  return adviceItem ? adviceItem.advice : ""; 
 };
-
 // Function to find the advice for humidity based on min and max values
 const getHumidityAdvice = (minHumidity, maxHumidity, adviceJSON) => {
   const adviceList = adviceJSON.advice["humidity_combined"];
@@ -96,7 +95,7 @@ const getHumidityAdvice = (minHumidity, maxHumidity, adviceJSON) => {
     maxHumidity >= item.range.maxima_min && maxHumidity <= item.range.maxima_max
   );
 
-  return adviceItem ? adviceItem.advice : ""; // Devuelve "" si no hay recomendación
+  return adviceItem ? adviceItem.advice : ""; 
 };
 
 // Function to load the advice from the JSON file
